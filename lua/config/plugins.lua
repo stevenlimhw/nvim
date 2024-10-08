4ec301306c0381ff74c54da6532cc56d5bcc6a51
Steven Lim <limhwsteven@gmail.com>
2024-10-08 20:47:22 +0800

    update config

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -71,5 +71,5 @@ require("lazy").setup({
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-  }
+  },
 })
