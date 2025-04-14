06dc689a44e0ba933e3bae1d051772d599339710
Steven Lim <limhwsteven@gmail.com>
2025-04-14 19:31:13 +0800

    update readme and fix some styling issues

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -73,14 +73,24 @@ require("lazy").setup({
     "vinnymeller/swagger-preview.nvim",
     run = "npm install -g swagger-ui-watcher",
   },
+  --{
+  --  "iamcco/markdown-preview.nvim",
+  --  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --  build = "cd app && yarn install",
+  --  init = function()
+  --    vim.g.mkdp_filetypes = { "markdown" }
+  --  end,
+  --  ft = { "markdown" },
+  --},
   {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    config = function()
+      vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
+    end,
   },
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
